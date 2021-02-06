95183337917df27544ee2132c59dc0b11145476e
Raymond Hunce <hunce18c@gmail.com>
2021-02-05 21:41:16 -0500

    Connect front-end Q&A to Q&A service

--- client/src/actions/QA/handleHelpfulQuestion.js
+++ client/src/actions/QA/handleHelpfulQuestion.js
@@ -1,7 +1,9 @@
-import searchAPI from '../../lib/searchEngine.js';
+// import searchAPI from '../../lib/searchEngine.js';
+import axios from 'axios';
 
 const handleHelpfulQuestion = (question_id) => {
-  return searchAPI.put(`qa/questions/${question_id}/helpful`)
+  // return searchAPI.put(`qa/questions/${question_id}/helpful`)
+  return axios.put(`qa/questions/${question_id}/helpful`)
     .then((data) => {
       console.log('Question marked as helpful', data);
     })
--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
 let app = require('./server.js');
 let PORT = process.env.PORT || 3333;
 
-app.listen( PORT, ()=>{ console.log(`listening on ${PORT}, visit: http://localhost:${PORT}`); });
+app.listen( PORT, () => { console.log(`listening on ${PORT}, visit: http://localhost:${PORT}`); });
